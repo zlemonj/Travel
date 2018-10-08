add74998bd3914eb90884fd90fe6f24deaa8517b
Jun_Zhang <1017805180@qq.com>
2018-10-08 16:48:41 +0800

    keep-alive

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,19 +9,19 @@
     </div>
     <router-link to="/city">
       <div class="header-right">
-        {{this.doubleCity}}
+        {{this.city}}
         <span class="iconfont arrow-icon">&#xe64a;</span>
       </div>
     </router-link>
   </div>
 </template>
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 export default{
   name: 'HomeHeader',
   computed: {
     ...mapState(['city']),
-    ...mapGetters(['doubleCity'])
+  /*  ...mapGetters(['doubleCity'])*/
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -8,9 +8,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state,
   mutations,
-  getters: {
+ /* getters: {
     doubleCity (state) {
       return state.city + ' ' + state.city
     }
-  }
+  }*/
 })
