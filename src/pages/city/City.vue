8a2ced360138b746e8aa1ba04911b3e538f7bd87
Jun_Zhang <1017805180@qq.com>
2018-10-08 10:08:49 +0800

    字母表滚动

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,13 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list 
+    :cities="cities" 
+    :hot="hotCities" 
+    :letter="letter"></city-list>
+    <city-alphabet 
+    :cities="cities" 
+    @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 <script>
@@ -23,7 +28,8 @@
     data (){
       return{
         cities:{},
-        hotCities: []
+        hotCities: [],
+        letter:''
       }
     },
     methods: {
@@ -38,6 +44,9 @@
           this.cities = data.cities
           this.hotCities = data.hotCities
         }
+      },
+      handleLetterChange (letter) {
+        this.letter = letter
       }
     },
     mounted () {
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,12 @@
             </div>
           </div>
       </div>
-      <div class="area" v-for="(item,key) of cities" :key="key">
+      <div 
+        class="area" 
+        v-for="(item,key) of cities" 
+        :key="key"
+        :ref="key"
+      >
         <div class="title boder-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
@@ -32,10 +37,19 @@
     name:'CityList',
     props:{
       hot: Array,
-      cities: Object
+      cities: Object,
+      letter:String
     },
     mounted () {
       this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    watch: {
+      letter () {
+        if (this.letter){
+          const element = this.$refs[this.letter][0]
+          this.scroll.scrollToElement(element)
+        }
+      }
     }
   }
 </script>
