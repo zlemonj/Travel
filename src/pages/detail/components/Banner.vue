910eb27687a52178f9a05022d1ad5836bdf6b66b
Jun_Zhang <1017805180@qq.com>
2018-10-09 17:38:55 +0800

    动画效果实现

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -10,12 +10,19 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+       <common-gallary
+         :imgs="bannerImgs"
+         v-show="showGallary"
+         @close="handleGallaryClose"
+       ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -37,7 +44,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
