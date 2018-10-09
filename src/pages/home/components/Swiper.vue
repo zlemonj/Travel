12e2afb4e22a350e0cb897426f4a8917abbfe7c9
Jun_Zhang <1017805180@qq.com>
2018-10-09 14:57:35 +0800

    画廊轮播完成

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -9,25 +9,25 @@
   </div>
 </template>
 <script>
-  export default {
-    name:'HomeSwiper',
-    props: {
-      list : Array
-    },
-    data () {
-      return {
-        swiperOption: {
-          pagination: '.swiper-pagination',
-          loop: true
-        }
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
       }
-    },
-        computed :{
-            showSwiper () {
-              return this.list.length
-        }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
+}
 </script>
 <style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
